dbf2c4396aadd33f4d3545ea01d52654b151ab18
Patrick Butkiewicz <patrick@intrepid.io>
2015-09-16 00:14:32 -0400

    Added base blastoff formula

---
+++ formula/blastoff.rb
@@ -0,0 +1,23 @@
+require 'formula'
+
+class Blastoff < Formula
+  homepage 'https://github.com/IntrepidPursuits/homebrew-blastoff'
+  url 'http://intrepidpursuits.github.io/homebrew-blastoff/Blastoff-__VERSION__.tar.gz'
+  sha1 '__SHA__'
+
+  depends_on 'xcproj' => :recommended
+
+  def install
+    prefix.install 'defaults', 'templates', 'vendor'
+    prefix.install 'lib' => 'rubylib'
+
+    man1.install ['man/blastoff.1']
+    man5.install ['man/blastoffrc.5']
+
+    bin.install 'src/blastoff'
+  end
+
+  test do
+    system "#{bin}/blastoff", '--version'
+  end
+end
