e078f2dccbb4d6a9ec67d1179b1aea856d504546
Patrick Butkiewicz <patrick@intrepid.io>
2015-09-19 17:17:30 -0400

    takeoff: Release version 1.0.beta

--- formula/takeoff.rb
+++ formula/takeoff.rb
@@ -3,14 +3,13 @@ require 'formula'
 class Takeoff < Formula
   homepage 'https://github.com/IntrepidPursuits/homebrew-takeoff'
   url 'http://intrepidpursuits.github.io/homebrew-takeoff/Takeoff-1.0.beta.tar.gz'
-  sha1 '57f1a0c0124a366786eee5e2c2e30586a1c4dbac'
+  sha1 'c12e76737122c53a732ed8587d8c9936ec21ed71'
 
   def install
     prefix.install 'vendor'
     prefix.install 'lib' => 'rubylib'
 
     man1.install ['man/takeoff.1']
-    man5.install ['man/takeoffrc.5']
 
     bin.install 'src/takeoff'
   end
