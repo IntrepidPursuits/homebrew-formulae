daa5eadbd7ce9a082a804db03adc89416641517e
Patrick Butkiewicz <patrick@intrepid.io>
2015-10-01 04:08:11 -0400

    blastoff: Release version 1.6.0.RC4.Intrepid

--- formula/blastoff.rb
+++ formula/blastoff.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Blastoff < Formula
   homepage 'https://github.com/IntrepidPursuits/homebrew-blastoff'
-  url 'http://intrepidpursuits.github.io/homebrew-blastoff/Blastoff-1.6.0.RC3.Intrepid.tar.gz'
-  sha1 '9fdf4888e269d0c45fd778ff0ead2b5f86ceee71'
+  url 'http://intrepidpursuits.github.io/homebrew-blastoff/Blastoff-1.6.0.RC4.Intrepid.tar.gz'
+  sha1 'dd4745566077d1fe9cc107c656f9a44c9443de79'
 
   depends_on 'xcproj' => :recommended
 
