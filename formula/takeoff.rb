0863db89b8ab738f8c00f0df9ebfda8c3a70cdf2
Patrick Butkiewicz <patrick@intrepid.io>
2017-11-15 13:24:20 -0500

    Update takeoff formula for sha256

--- formula/takeoff.rb
+++ formula/takeoff.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Takeoff < Formula
   homepage 'https://github.com/IntrepidPursuits/homebrew-takeoff'
   url 'http://intrepidpursuits.github.io/homebrew-takeoff/Takeoff-1.0.RC2.tar.gz'
-  sha1 '555979a71bec283ab81dc30a1d4c5457423e8b6d'
+  Formula.sha256 '10cad405e986e6adfd6fdbc39a4b2ddb1e53b7478a31594efb9dfc7e8a7bd6d0'
 
   def install
     prefix.install 'vendor'
